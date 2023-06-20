c842cc87f334aece4f17ab9eb73ad7735fbdddd8
Lucas Santana Lemos <lslemos59@gmail.com>
2023-06-20 13:52:26 -0300

    Tarefas done

--- commands/ListaTarefas.py
+++ commands/ListaTarefas.py
@@ -17,15 +17,17 @@ class Buttons(discord.ui.View):
         self.tarefas_file.write(f"> {self.msg.capitalize()}\n")
         
         self.tarefas_file = open(f"./database/Tarefas/{interaction.user.name}.txt", "r")
+        self.tarefas_concluidas_file = open(f"./database/Tarefas/Concluidas/{interaction.user.name}.txt", "r")
         
         
         embed_Titulo = discord.Embed(title=f"Sua Lista de Tarefas", description="Perfeito para monitorar e controlar suas atividades!", color=discord.Color.green())
         embed_Titulo.set_thumbnail(url=interaction.user.avatar.url)
         embed_Tarefas = discord.Embed(title=f":blue_book: Atividades de @{interaction.user.name.capitalize()}", description=f"Voce pode adicionar, remover e gerenciar por este embed", color=discord.Color.blurple())
-        embed_Tarefas.add_field(inline=True, name=f"Lista a cumprir:\n", value=f"\n{self.tarefas_file.read()}")
-        embed_Tarefas.add_field(inline=True, name=f"Já completadas:\n", value=f"")
+        embed_Tarefas.add_field(inline=True, name=f"Lista a cumprir:", value=f"{self.tarefas_file.read()}")
+        embed_Tarefas.add_field(inline=True, name=f"Já completadas:", value=f"{self.tarefas_concluidas_file.read()}")
         embeds = [embed_Titulo, embed_Tarefas]
-        self.view = Buttons()
+        
+        self.view = Buttons(timeout=None)
         
         self.add = await interaction.channel.send("Sua tarefa está sendo **adicionada**...")
         self.msg_list = [self.tarefa, self.adicionar_lista, self.add]
@@ -34,7 +36,54 @@ class Buttons(discord.ui.View):
         await interaction.channel.send(embeds=embeds, view=self.view)
         
 
-
+    @discord.ui.button(label="Concluir Tarefa", emoji="📝",style=discord.ButtonStyle.blurple)
+    async def concluir_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.defer(ephemeral=True)
+        
+        self.bot_msg1 = await interaction.channel.send("Digite a tarefa que voce concluiu")    
+        self.tarefa_concluida = await interaction.client.wait_for("message")
+        self.tarefas_list = []
+        self.emoji_concluido = ":white_check_mark:"
+        
+        with open(f"./database/Tarefas/{interaction.user.name}.txt", "r") as arquivo:
+            for tarefa in arquivo:
+                self.tarefas_list.append(tarefa[2:-1])
+                
+          
+        if self.tarefa_concluida.content.capitalize() in self.tarefas_list:
+            self.tarefas_list.remove(self.tarefa_concluida.content.capitalize())  
+            
+        self.bot_msg2 = await interaction.channel.send(f"Sua tarefa está sendo marcada como **concluida**...")   
+        
+        with open(f"./database/Tarefas/{interaction.user.name}.txt", "w") as arquivo:
+            for item in self.tarefas_list:
+                arquivo.write(f"> {item}\n")
+                
+        self.tarefas_file = open(f"./database/Tarefas/{interaction.user.name}.txt", "r")
+        
+        self.tarefas_concluidas_file = open(f"./database/Tarefas/Concluidas/{interaction.user.name}.txt", "a")
+        self.tarefas_concluidas_file.write(f"{self.emoji_concluido} {self.tarefa_concluida.content.capitalize()}\n")
+        self.tarefas_concluidas_file = open(f"./database/Tarefas/Concluidas/{interaction.user.name}.txt", "r")
+        
+        embed_Titulo = discord.Embed(title=f"Sua Lista de Tarefas", description="Perfeito para monitorar e controlar suas atividades!", color=discord.Color.green())
+        embed_Titulo.set_thumbnail(url=interaction.user.avatar.url)
+        embed_Tarefas = discord.Embed(title=f":blue_book: Atividades de @{interaction.user.name.capitalize()}", description=f"Voce pode adicionar, remover e gerenciar por este embed", color=discord.Color.blurple())
+        
+        if len(self.tarefas_list) == 0:
+            embed_Tarefas.add_field(inline=True, name=f"Você ainda não possui tarefas", value=f"")
+        else:
+            embed_Tarefas.add_field(inline=True, name=f"Lista a cumprir:", value=f"\n{self.tarefas_file.read()}")
+            embed_Tarefas.add_field(inline=True, name=f"Já completadas:", value=f"{self.tarefas_concluidas_file.read()}")
+                
+        embeds = [embed_Titulo, embed_Tarefas]    
+        self.msg_list = [self.bot_msg1, self.bot_msg2, self.tarefa_concluida]
+        self.view = Buttons(timeout=None)
+            
+        await interaction.delete_original_response()
+        await interaction.channel.delete_messages(messages=self.msg_list)           
+        await interaction.channel.send(embeds=embeds, view=self.view)
+        
+        
     @discord.ui.button(label="Remover Tarefa", emoji="🔧",style=discord.ButtonStyle.danger)
     async def remover_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer(ephemeral=True)
@@ -49,9 +98,7 @@ class Buttons(discord.ui.View):
         with open(f"./database/Tarefas/{interaction.user.name}.txt", "r") as arquivo:
             for tarefa in arquivo:
                 self.tarefas_list.append(tarefa[2:-1])
-                
-        
-        
+                       
         if self.tarefa_remover.content.capitalize() in self.tarefas_list:
             
             self.tarefas_file = open(f"./database/Tarefas/{interaction.user.name}.txt", "r")
@@ -61,19 +108,22 @@ class Buttons(discord.ui.View):
                 for item in self.tarefas_list:
                     arquivo.write(f"> {item}\n")
             
+            self.tarefas_concluidas_file = open(f"./database/Tarefas/Concluidas/{interaction.user.name}.txt", "r")
+            
             embed_Titulo = discord.Embed(title=f"Sua Lista de Tarefas", description="Perfeito para monitorar e controlar suas atividades!", color=discord.Color.green())
             embed_Titulo.set_thumbnail(url=interaction.user.avatar.url)
             embed_Tarefas = discord.Embed(title=f":blue_book: Atividades de @{interaction.user.name.capitalize()}", description=f"Voce pode adicionar, remover e gerenciar por este embed", color=discord.Color.blurple())
             
             if len(self.tarefas_list) == 0:
                 embed_Tarefas.add_field(inline=True, name=f"Você ainda não possui tarefas", value="")
+                embed_Tarefas.add_field(inline=True, name=f"Já completadas:", value=f"{self.tarefas_concluidas_file.read()}")
             else:
                 embed_Tarefas.add_field(inline=True, name=f"Lista a cumprir:", value=f"\n{self.tarefas_file.read()}")
-                embed_Tarefas.add_field(inline=True, name=f"Já completadas:", value=f"")
+                embed_Tarefas.add_field(inline=True, name=f"Já completadas:", value=f"{self.tarefas_concluidas_file.read()}")
         
         
             embeds = [embed_Titulo, embed_Tarefas]
-            self.view = Buttons()
+            self.view = Buttons(timeout=None)
             
             self.remove = await interaction.channel.send("Sua tarefa está sendo **removida**...")
             self.msg_list = [self.msg1, self.tarefa_remover, self.remove]
@@ -96,7 +146,8 @@ class ListadeTarefas(commands.Cog):
         await interaction.response.defer(ephemeral=False)
         
         self.content = []
-        self.aviso = ""
+        
+        
         self.fazer = open(f"./database/Tarefas/{interaction.user.name}.txt", "a")
         self.fazer = open(f"./database/Tarefas/{interaction.user.name}.txt", "r")
         
@@ -109,16 +160,19 @@ class ListadeTarefas(commands.Cog):
             for item in arquivo:
                 self.content.append(item)
                 
+        self.tarefas_concluidas_file = open(f"./database/Tarefas/Concluidas/{interaction.user.name}.txt", "r")
+                
         if len(self.content) == 0:
             embed_Tarefas.add_field(inline=True, name=f"Você ainda não possui tarefas", value="")
+            embed_Tarefas.add_field(inline=True, name=f"Já completadas:", value=f"{self.tarefas_concluidas_file.read()}")
         else:
             embed_Tarefas.add_field(inline=True, name=f"Lista a cumprir:", value=f"\n{self.fazer.read()}")
-            embed_Tarefas.add_field(inline=True, name=f"Já completadas:", value=f"")
+            embed_Tarefas.add_field(inline=True, name=f"Já completadas:", value=f"{self.tarefas_concluidas_file.read()}")
         
         
         embeds = [embed_Titulo, embed_Tarefas]
         
-        self.view = Buttons()
+        self.view = Buttons(timeout=None)
         
         await interaction.followup.send(embeds=embeds, view=self.view)
            
--- teste.py
+++ teste.py
@@ -1,9 +1,8 @@
 content = []
 check_list = []
 
-with open("database/Tarefas/tarefas.txt", "r") as arquivo:
+with open("database/Tarefas/lucasom.txt", "r") as arquivo:
     for item in arquivo:
-
         content.append(item[2:-1])
     
 print(content)
